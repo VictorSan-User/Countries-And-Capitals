c6a4f574576f2ac0bc10b002d6d53bf2df6ba873
Victor <victorhenriquesan@outlook.com>
2025-02-07 23:48:32 -0300

    003

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [MainController::class, 'showData']);
+Route::get('/', [MainController::class, 'startGame'])->name('star_game');
+Route::post('/', [MainController::class, 'prepareGame'])->name('prepare_game');
+
+//no jogo
+Route::get('/game', [MainController::class, 'game'])->name('game');
